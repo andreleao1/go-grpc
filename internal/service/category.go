4962feb527751e1228d6cfa7e0fc9c0a086fd9e1
andre.leao <andre.silva@willbank.com.br>
2022-12-22 11:48:05 -0300

    Initial commit

---
+++ internal/service/category.go
@@ -0,0 +1,36 @@
+package service
+
+import (
+	"context"
+
+	"exemple.com/grpc/internal/database"
+	"exemple.com/grpc/internal/pb"
+)
+
+type CategoryService struct {
+	pb.UnimplementedCategoryServiceServer
+	CategoryDB database.Category
+}
+
+func NewCategoryService(categoryDB database.Category) *CategoryService {
+	return &CategoryService{
+		CategoryDB: categoryDB,
+	}
+}
+
+func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCategoryRequest) (*pb.CategoryResponse, error) {
+	category, error := c.CategoryDB.Create(in.Name, in.Description)
+	if error != nil {
+		return nil, error
+	}
+
+	categoryResponse := &pb.Category{
+		Id:          category.ID,
+		Name:        category.Name,
+		Description: category.Description,
+	}
+
+	return &pb.CategoryResponse{
+		Category: categoryResponse,
+	}, nil
+}
